660ab487b3debd10e50d590cb876d53ee1b96dda
NetSMG <150570330+netsmg@users.noreply.github.com>
2024-01-31 21:00:10 +0600

    Create +page.svelte

---
+++ src/routes/contact/+page.svelte
@@ -0,0 +1,87 @@
+<script>
+  import { onMount } from "svelte";
+  import { getDatabase, ref, push } from "firebase/database";
+
+  let formData = {
+    name: "",
+    email: "",
+    subject: "", // Added subject field
+    message: "",
+  };
+
+  const handleSubmit = async () => {
+    const db = getDatabase();
+    const contactsRef = ref(db, "contacts");
+
+    // Push the form data to the "contacts" collection
+    await push(contactsRef, formData);
+
+    // Clear the form after submission
+    formData = {
+      name: "",
+      email: "",
+      subject: "",
+      message: "",
+    };
+
+    // Alert after submission
+    alert("Form submit successful!");
+
+    console.log("Form submitted to Firebase Realtime Database!");
+  };
+
+  onMount(() => {
+    document.title = "Contact";
+  });
+</script>
+
+<!-- page title -->
+<section class="section section--first section--last section--head" data-bg="img/bg.jpg">
+  <div class="container">
+    <div class="row">
+      <div class="col-12">
+        <div class="section__wrap">
+          <!-- section title -->
+          <h2 class="section__title">Contact Us</h2>
+          <!-- end section title -->
+
+          <!-- breadcrumb -->
+          <ul class="breadcrumb">
+            <li class="breadcrumb__item"><a href="index.html">Home</a></li>
+            <li class="breadcrumb__item breadcrumb__item--active">Contact</li>
+          </ul>
+          <!-- end breadcrumb -->
+        </div>
+      </div>
+    </div>
+  </div>
+</section>
+<!-- end page title -->
+
+<!-- section -->
+<section class="section">
+  <div class="container">
+    <div class="row">
+      <div class="col-12 col-md-7 col-xl-8">
+        <div class="row">
+          <!-- section title -->
+          <div class="col-12">
+            <h2 class="section__title">Get in Touch</h2>
+          </div>
+          <!-- end section title -->
+
+          <div class="col-12">
+            <form on:submit={handleSubmit} class="form form--contacts">
+              <input type="text" class="form__input" id="name" bind:value={formData.name} placeholder="Your Name">
+              <input type="text" class="form__input" id="email" bind:value={formData.email} placeholder="Your Email">
+              <input type="text" class="form__input" id="subject" bind:value={formData.subject} placeholder="Subject">
+              <textarea name="text" class="form__textarea" id="message" rows="4" bind:value={formData.message} placeholder="Type your message..."></textarea>
+              <button type="submit" class="form__btn">Send Message</button>
+            </form>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+</section>
+<!-- end section -->
