7da56e7a1c779d7d6b915ecfa982703f106fe5d4
NetSMG <150570330+netsmg@users.noreply.github.com>
2024-01-31 21:04:15 +0600

    Update +page.svelte

--- src/routes/contact/+page.svelte
+++ src/routes/contact/+page.svelte
@@ -36,26 +36,7 @@
 </script>
 
 <!-- page title -->
-<section class="section section--first section--last section--head" data-bg="img/bg.jpg">
-  <div class="container">
-    <div class="row">
-      <div class="col-12">
-        <div class="section__wrap">
-          <!-- section title -->
-          <h2 class="section__title">Contact Us</h2>
-          <!-- end section title -->
 
-          <!-- breadcrumb -->
-          <ul class="breadcrumb">
-            <li class="breadcrumb__item"><a href="index.html">Home</a></li>
-            <li class="breadcrumb__item breadcrumb__item--active">Contact</li>
-          </ul>
-          <!-- end breadcrumb -->
-        </div>
-      </div>
-    </div>
-  </div>
-</section>
 <!-- end page title -->
 
 <!-- section -->
